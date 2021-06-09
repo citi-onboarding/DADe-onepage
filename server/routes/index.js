987f940fff09938e3189b7a72700ccb232bdad43
Guilherme <gfxs@cin.ufpe.br>
2021-06-09 17:02:37 -0300

    feat(main): refactor controllers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,17 @@
 const path = require('path');
-const keystone = require('keystone');
 const cors = require('cors');
-
-const Post = keystone.list('Posts');
+const postController = require('../controllers/postController');
 
 module.exports = (app) => {
   app.use(cors());
+
+  app.get('/api/posts', postController.getPostController);
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  app.get('/api/posts', (req, res) => {
-    Post.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
-    });
-  });
-
   app.get('*', (req, res) => {
-		res.redirect('/');
-	});
+    res.redirect('/');
+  });
 };
