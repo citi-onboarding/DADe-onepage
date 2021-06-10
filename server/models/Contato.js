ba43c7a229bd79802f789d823ea23447a9b3bcd5
Marcos André <macam@cin.ufpe.br>
2021-06-10 20:21:44 -0300

    feat(feature/model-contato): added nodelete true in the model List properties

--- server/models/Contato.js
+++ server/models/Contato.js
@@ -2,7 +2,7 @@ const keystone = require('keystone');
 
 const { Types } = keystone.Field;
 
-const Contato= new keystone.List('Contato', {nocreate: true});
+const Contato= new keystone.List('Contato', {nocreate: true, nodelete: true});
 
 Contato.add({
   instagram: {
