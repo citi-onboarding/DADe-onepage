18e3d8c2a189b0413f843b35fccdc8b49069d63a
Marcos André <macam@cin.ufpe.br>
2021-06-16 11:47:52 -0300

    feat(feature/view-contact): created contact view (without forms)

---
+++ client/src/Views/Contact/Contact.jsx
@@ -0,0 +1,52 @@
+import {
+    PostList,
+    Card,
+    Paragraph,
+    FooterNotion,
+    CardFather,
+    Title,
+    Text,
+    SmallText,
+    ContactLine,
+  } from '../../components';
+  
+  import { useState, useEffect } from 'react';
+  
+  import Instagram from '../../Assets/InstaIcon.svg';
+  import Twitter from '../../Assets/TwitterIcon.svg';
+  import Mail from '../../Assets/MailIcon.svg';
+  import Telegram from '../../Assets/TelegramIcon.svg';
+  import Whatsapp from '../../Assets/ZapIcon.svg';
+  
+  import './Contact.css';
+  
+  function Contact(props) {
+  
+      return (
+        <section className='contact'>
+          <div className='contact-info'>
+            <div className='contact-paragraph'>
+              <div className='title-div'>
+              <Title titleText='Entre em Contato'/>
+              </div>
+              <div className='small-text-div'>
+              <SmallText pSmallText='Acompanhe nossas redes sociais e fique por dentro do que estamos preparando' lineHeight='25px' />
+              </div>
+            </div>
+            <div className='line-group-div'>
+              <ContactLine lineImg={Instagram} contactInfo='dadeupe' contactLink ='https://ge.globo.com/pe/futebol/times/nautico/'/>
+              <ContactLine lineImg={Twitter} contactInfo='DADeUFPE' contactLink ='https://ge.globo.com/pe/futebol/times/nautico/'/>
+              <ContactLine lineImg={Mail} contactInfo='dadeufpe@gmail.com' contactLink ='https://ge.globo.com/pe/futebol/times/nautico/'/>
+              <ContactLine lineImg={Telegram} contactInfo='Telegram' contactLink ='https://ge.globo.com/pe/futebol/times/nautico/'/>
+              <ContactLine lineImg={Whatsapp} contactInfo='Whatsapp' contactLink ='https://ge.globo.com/pe/futebol/times/nautico/'/>
+            </div>
+          </div>
+          <div className='forms'>
+            <form action="">
+            </form>
+          </div>
+        </section>
+      );
+    }
+  
+  export default Contact;
--- client/src/Views/index.jsx
+++ client/src/Views/index.jsx
@@ -2,3 +2,4 @@ export { default as Organization } from './Organization';
 export { default as Banner } from './Banner';
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
+export { default as Contact} from './Contact';
