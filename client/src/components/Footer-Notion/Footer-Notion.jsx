e630bb646bc5c79619ee438b2531a444b679e251
Marcos André <macam@cin.ufpe.br>
2021-06-17 11:26:57 -0300

    feat(feature/create-notion-link-backend-connection): added notion link connection to the backend

--- client/src/components/Footer-Notion/Footer-Notion.jsx
+++ client/src/components/Footer-Notion/Footer-Notion.jsx
@@ -6,11 +6,24 @@ import './Footer-Notion.css';
 import Button from '../Button';
 
 function Footer(props) {
+  const [notion, setNotion] = useState([]);
+
+  const loadNotion = async () => {
+    const res = await axios.get('http://localhost:3001/api/notion');
+    setNotion(res.data);
+  };
+
+  useEffect(() => {
+    loadNotion();
+  }, []);
 
   return (
     <div className="footer-div">
       <h1>{props.footerTitle}</h1>
-      <Button link="https://www.notion.so/" text="Acesse nosso Notion" className="Notion-Buttom"/>
+      {notion?.map(({_id, link})  => (
+      <Button link={link} text="Acesse nosso Notion" className="Notion-Buttom"/>
+      ))}
+      
     </div>
   );
 }
