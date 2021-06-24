e26187489c8fa9e378c72cb62517d53587f693b3
Marcos André <macam@cin.ufpe.br>
2021-06-24 09:29:08 -0300

    feat(feature/create-organization-view): made realizations view responsive (added complete mobile support)

--- client/src/Views/Realizations/Realizations.jsx
+++ client/src/Views/Realizations/Realizations.jsx
@@ -35,6 +35,26 @@ function Realizations() {
         slidesToScroll: 1,
         nextArrow: <NextArrow/>,
         prevArrow: <PrevArrow/>,
+        responsive: [
+          {
+            breakpoint: 1025,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 732,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+        ]
       };
     
     return (
