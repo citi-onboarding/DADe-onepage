c84e91af2eec3bb26011ac8d30493f494350d471
Guilherme <gfxs@cin.ufpe.br>
2021-06-24 19:41:45 -0300

    refiz o build e funcionou

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === "production";
 
 export default {
-  url: prod ? 'https://dade-onepage.herokuapp.com' : 'http://localhost:3001',
+  url: 'http://localhost:3001',
 }
