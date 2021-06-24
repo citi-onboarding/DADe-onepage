eb02196663c3994c486f08e66ebca42809984b28
Pedro <pccql@cin.ufpe.br>
2021-06-24 15:58:23 -0300

    deploy

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === "production";
 
 export default {
-  url: prod ? 'https://dade-onepage.herokuapp.com' : 'http://localhost:3001',
+  url: prod ? 'https://dade-ufpe.herokuapp.com' : 'http://localhost:3001',
 }
